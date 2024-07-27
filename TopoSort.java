13b29d29b283cd98cdab152fe17b15c2d8c523ee
Jaideep12 <jaideeps85@gmail.com>
2024-07-27 16:24:23 +0530

    Create TopoSort.java

---
+++ TopoSort.java
@@ -0,0 +1,37 @@
+//This works only for DAG
+//Idea is to first store data in adjacency list and then go node by node by keeping every node in a stack
+class TopoSort
+{
+    //Function to return list containing vertices in Topological order. 
+    static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) 
+    {
+        int [] visited = new int [V];
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int i=0;i<V;i++){
+            if(visited[i]==0){
+                dfs(V,i,adj,stack,visited);
+            }
+        }
+        
+        int result[] = new int [V];
+        int x=0;
+        while(!stack.isEmpty()){
+            result[x++] = stack.pop();
+        }
+        return result;
+    }
+    
+   static void dfs(int V, int node, ArrayList<ArrayList<Integer>> adj, Stack<Integer> stack
+    ,int [] visited){
+        visited[node] = 1;
+        
+        for(int a : adj.get(node)){
+            if(visited[a]==0){
+                dfs(V,a,adj,stack,visited);
+            }
+        }
+        stack.push(node);
+    }
+}
