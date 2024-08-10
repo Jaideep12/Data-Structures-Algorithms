21feef141649d0648a665dc5d46784a2c2517ab5
Jaideep12 <jaideeps85@gmail.com>
2024-08-10 08:05:04 +0530

    Create MaxPathSum.java

---
+++ BinaryTrees/MaxPathSum.java
@@ -0,0 +1,15 @@
+class Solution {
+    int maxSum=Integer.MIN_VALUE;
+    private int helper(TreeNode root){
+        if(root==null) return 0;
+        int left=Math.max(0,helper(root.left));
+        int right=Math.max(0,helper(root.right));
+        maxSum=Math.max(maxSum,left+right+root.val);
+
+        return Math.max(left,right)+root.val;
+    }
+    public int maxPathSum(TreeNode root) {
+        helper(root);
+        return maxSum;
+    }
+}
