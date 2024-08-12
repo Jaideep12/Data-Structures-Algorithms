557688ef0ff066b2d5cccede1eb9af8ee41231eb
Jaideep12 <jaideeps85@gmail.com>
2024-08-12 07:12:53 +0530

    Update PreInOrderBT.java

--- BinaryTrees/PreInOrderBT.java
+++ BinaryTrees/PreInOrderBT.java
@@ -1,6 +1,7 @@
 // 105. Construct Binary Tree from Preorder and Inorder Traversal
 // Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
-
+// Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+// Output: [3,9,20,null,null,15,7]
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {    
         HashMap<Integer,Integer> inMap = new HashMap<>();
