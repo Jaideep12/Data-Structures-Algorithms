5b9b212906ddeee9261907db92dd3abc2fea3f4f
Jaideep12 <jaideeps85@gmail.com>
2024-08-17 19:28:55 +0530

    Create Search2DMatrix.java

---
+++ Striver SDE Sheet/Search2DMatrix.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int left = 0;
+        int right = m * n -1;
+
+        while(left<=right){
+            int midpoint = left + (right-left)/2;
+
+            int element = matrix[midpoint/n][midpoint%n];
+            if(element == target){
+                return true;
+            }else if(target > element){
+                left = midpoint+1;
+            }else{
+                right = midpoint-1;
+            }
+        }
+        return false;
+    }
+}
