367ce13064411b7d22de6b62726ec01e6569ced5
Jaideep12 <jaideeps85@gmail.com>
2024-08-21 07:00:41 +0530

    Create Add2NumbersLL.java

---
+++ Striver SDE Sheet/Add2NumbersLL.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode (-1);
+        ListNode ans = dummy;
+        ListNode t1=l1;
+        ListNode t2=l2;
+        int carry=0;
+        while(t1!=null && t2!=null){
+            int sum = t1.val + t2.val + carry;
+            carry = sum/10;
+            sum=sum%10;
+            ListNode t = new ListNode(sum);
+            ans.next=t;
+            ans=ans.next;
+
+            t1=t1.next;
+            t2=t2.next;
+        }
+
+        if(t1!=null){
+            while(t1!=null){
+                int sum = t1.val + carry;
+                carry = sum/10;
+                sum=sum%10;
+                ListNode t = new ListNode(sum);
+                ans.next=t;
+                ans=ans.next;
+
+                t1=t1.next;
+            }
+        }else if(t2!=null){
+            while(t2!=null){
+                int sum = t2.val + carry;
+                carry = sum/10;
+                sum=sum%10;
+                ListNode t = new ListNode(sum);
+                ans.next=t;
+                ans=ans.next;
+
+                t2=t2.next;
+            }
+        }
+        if(carry>0){
+            ListNode t = new ListNode(carry);
+            ans.next = t;
+            ans=ans.next;
+        }
+        return dummy.next;
+    }
+}
